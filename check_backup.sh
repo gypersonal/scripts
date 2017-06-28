d95d93ac4e9fea01ce2434a1907e7f971d771968
gypersonal <gypersonal@gmail.com>
2017-06-29 00:57:06 +0300

    Update check_backup.sh

--- check_backup.sh
+++ check_backup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-date=`date +%F`
-
-if [[ -f ./backups/$date/backup.tar.gz ]]; then
+backup_date=`date +%F`
+backup_dir=/home/ygromov/scripts/backups
+backup_file=backup.tar.gz
+if [[ -f $backup_dir/$backup_date/$backup_file ]]; then
   echo true
 else
   echo false
